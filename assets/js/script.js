e3b1f45c81e88da53f55a9226c5d06228c679159
Jessica <98659450+JessicaSaantos@users.noreply.github.com>
2022-05-17 15:15:37 -0300

    add

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+function mandaZap(msg) {
+    let text = msg.firstElementChild.innerText // Capturando o primeiro filho da tag pai 
+    //e o seu conteudo no caso o texto
+    let numero = prompt('Qual é o número?')
+    let zapLink = `https://api.whatsapp.com/send?phone=55${numero}&text=${text}`
+
+    return window.open(zapLink)
+}
